feca606523d0e3a9e3136176da3fd347f50218ec
Michael Armijo <marmijo1@uci.edu>
2024-11-04 23:58:14 -0800

    made it so that it produces our report even if it crashes

--- launch.py
+++ launch.py
@@ -20,5 +20,7 @@ if __name__ == "__main__":
     parser.add_argument("--restart", action="store_true", default=False)
     parser.add_argument("--config_file", type=str, default="config.ini")
     args = parser.parse_args()
-    main(args.config_file, args.restart)
-    write_report()
+    try:
+        main(args.config_file, args.restart)
+    finally:
+        write_report()
